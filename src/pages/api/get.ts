64f733c166eda5a8175c2ab347864165d1eaf766
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-06 00:19:46 +0530

    not working

--- src/pages/api/get.ts
+++ src/pages/api/get.ts
@@ -4,15 +4,17 @@ import sample from "@/utils/sample";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   type book = {
     name: string;
+    question:string;
   };
-  const { name } = req.body as book;
+  const { name , question } = req.body as book;
   const response = await openAi.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       { role: "system", content: `${sample.system}` },
       { role: "user", content: `${sample.user1}` },
       { role: "assistant", content: `${sample.assistant}` },
-      { role: "user", content: `${message}` },
+      { role: "user", content: `You are book ${name}. answer this question .
+      Can you explain ${question}` },
     ],
   });
   res.status(200).json({ name: response.data.choices[0].message?.content });
