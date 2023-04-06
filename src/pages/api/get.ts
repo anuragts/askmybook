a75a3605953e1e42e4f992c2bef931d5b679106d
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-06 18:40:43 +0530

    improvement

--- src/pages/api/get.ts
+++ src/pages/api/get.ts
@@ -3,19 +3,22 @@ import { openAi } from "@/config/openAi.config";
 import sample from "@/utils/sample";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   type book = {
-    name: string;
+    book:string;
+    author:string;
     question:string;
   };
-  const { name , question } = req.body as book;
+  const { book , question , author } = req.body as book;
   const response = await openAi.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       { role: "system", content: `${sample.system}` },
       { role: "user", content: `${sample.user1}` },
       { role: "assistant", content: `${sample.assistant}` },
-      { role: "user", content: `You are book ${name}. answer this question .
+      { role: "user", content: `The title of the book is ${book} by ${author || ""}.  answer this question .
       Can you explain ${question}` },
     ],
   });
-  res.status(200).json({ name: response.data.choices[0].message?.content });
+
+  // console.log(`${book} by ${author} and ${question} `)
+  res.status(200).json(response.data.choices[0].message?.content || "");
 };
